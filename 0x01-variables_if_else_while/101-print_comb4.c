fd6ba301257cb46ed98012e3608fda143c918bbd
Thormass007 <thomasuwaya@gmail.com>
2022-12-12 08:35:41 -0800

    ADV TASK 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,8 @@ for (tens = '0'; tens <= '9'; tens++)
 {
 for (ones = '0'; ones <= '9'; ones++)
 {
-if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))/*eliminates repetition*/
+if (!((ones == tens) || (tens == hundreds) ||
+(tens > ones) || (hundreds > tens)))/*eliminates repetition*/
 {
 putchar(hundreds);
 putchar(tens);
